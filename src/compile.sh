292a0ee25c0870dc0f509af96a3b980f718e7034
Nik Reljin <nikola.reljin@dowjones.com>
2018-11-15 10:19:32 -0500

    Commpile on changes in package.json or composer.json

--- src/compile.sh
+++ src/compile.sh
@@ -7,10 +7,14 @@ print_important "Compiling the dependencies"
 changedFiles="$(git diff-tree -r --name-only --no-commit-id HEAD)"
 
 runOnChange() {
-	echo "$changedFiles" | grep -q "$1" && eval "$2"
+	print_important "$changedFiles" | grep -q "$1" && eval "$2"
 }
 
-runOnChange package-lock.json "npm install"
-runOnChange composer.lock "composer install"
+runOnChange package.json "npm install && npm run build"
+runOnChange composer.json "composer install && composer dump-autoload --optimize"
+
+runOnChange package-lock.json "npm install && npm run build"
+runOnChange composer.lock "composer install && composer dump-autoload --optimize"
+
 
 print_success "Compile process completed."
